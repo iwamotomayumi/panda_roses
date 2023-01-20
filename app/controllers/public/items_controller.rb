5cfb7b8770635c82da5a1647600852e12650f526
EC2 Default User <ec2-user@ip-172-31-4-31.ap-northeast-1.compute.internal>
2023-01-20 18:06:01 +0000

    全体のレイアウト、フォント、背景画像変更

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,6 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @items = Item.order(created_at: :desc).page(params[:page]).per(10)
   end
