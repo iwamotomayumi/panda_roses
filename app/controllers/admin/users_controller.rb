282f6e3d2545455aaea3e9f6fa2a1d943295eed5
EC2 Default User <ec2-user@ip-172-31-4-31.ap-northeast-1.compute.internal>
2023-01-24 15:55:17 +0000

    [admin,public,css]レイアウト、文字修正２

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,8 +26,11 @@ class Admin::UsersController < ApplicationController
   end
 
 
+  private
+
   def user_params
     params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :introduction, :telephone_number, :is_deleted, :profile_image)
   end
+
 
 end
