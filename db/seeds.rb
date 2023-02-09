65495ae7fa09875706d4b257c0b3a149a020243a
EC2 Default User <ec2-user@ip-172-31-4-31.ap-northeast-1.compute.internal>
2023-02-09 14:27:25 +0000

    [README]テーマ選定、サイトURL説明修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,18 @@ Admin.create!(
    password: 'hibiki'
 )
 
+users = User.create!(
+  [
+    {email: 'hanako@test.com', last_name: '日本', first_name: '花子', last_name_kana: 'ニホン', first_name_kana: 'ハナコ', telephone_number: '00000000000', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'james@test.com', name: 'James', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'lucas@test.com', name: 'Lucas', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
+
+PostImage.create!(
+  [
+    {title: '記念日', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), caption: '記念日にもらいました', user_id: users[0].id },
+    {title: 'サプライズ', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), caption: '友達にプレゼント！', user_id: users[1].id },
+    {title: '一目惚れで購入', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), caption: 'こういうの大好き！', user_id: users[2].id }
+  ]
+)
