9808bbe7c0cefeee36c00fad4576e741d5f6427a
EC2 Default User <ec2-user@ip-172-31-4-31.ap-northeast-1.compute.internal>
2023-02-10 02:59:36 +0000

    seeds.rbテストデータ追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,24 @@ Admin.create!(
 
 users = User.create!(
   [
-    {email: 'hanako@test.com', last_name: '日本', first_name: '花子', last_name_kana: 'ニホン', first_name_kana: 'ハナコ', telephone_number: '00000000000', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
-    {email: 'james@test.com', name: 'James', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
-    {email: 'lucas@test.com', name: 'Lucas', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+    {email: 'hanako@test.com', last_name: '山田', first_name: '花子', last_name_kana: 'ヤマダ', first_name_kana: 'ハナコ', telephone_number: '00000000000', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'miyako@test.com', last_name: '沖縄', first_name: '都', last_name_kana: 'オキナワ', first_name_kana: 'ミヤコ', telephone_number: '00000000000', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'ken@test.com', last_name: '山口', first_name: 'けん', last_name_kana: 'ヤマグチ', first_name_kana: 'ケン', telephone_number: '00000000000', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
   ]
 )
 
 PostImage.create!(
   [
-    {title: '記念日', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), caption: '記念日にもらいました', user_id: users[0].id },
-    {title: 'サプライズ', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), caption: '友達にプレゼント！', user_id: users[1].id },
-    {title: '一目惚れで購入', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), caption: 'こういうの大好き！', user_id: users[2].id }
+    {title: 'マカロン', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), caption: 'バラを練り込んだマカロンです！', user_id: users[0].id },
+    {title: '結婚式', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), caption: '素敵な花輪とブーケ作ってもらった！', user_id: users[1].id },
+    {title: 'プレゼント', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), caption: 'プレゼント貰って嬉しい！', user_id: users[2].id }
+  ]
+)
+
+Item.create!(
+  [
+    {name: '紫陽花', price: '500', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-item1.jpg"), filename:"sample-item1.jpg"), introduction: '紫陽花のドライフラワーです'},
+    {name: 'リース', price: '1000', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-item2.jpg"), filename:"sample-item2.jpg"), introduction: '文字は変更できます'},
+    {name: 'ブーケ', price: '500', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-item3.jpg"), filename:"sample-item3.jpg"), introduction: '小さいサイズのブーケです'}
   ]
 )
